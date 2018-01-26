ace5de66ce4c851f325e7acdccc1d050cb2ddeac
kfrn <knfrances@gmail.com>
2018-01-26 22:34:15 +1300

    Add navbar and footer; general setup

--- src/Model.elm
+++ src/Model.elm
@@ -1,10 +1,18 @@
 module Model exposing (Model, init)
 
+import Types exposing (..)
+
 
 type alias Model =
-    {}
+    { system : SystemOfMeasurement }
 
 
 init : ( Model, Cmd msg )
 init =
-    ( {}, Cmd.none )
+    ( initialModel, Cmd.none )
+
+
+initialModel : Model
+initialModel =
+    { system = Imperial
+    }
--- src/View.elm
+++ src/View.elm
@@ -1,14 +1,46 @@
 module View exposing (view)
 
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, em, h1, i, nav, p, span, text)
+import Html.Attributes exposing (attribute, class, id)
 import Model exposing (Model)
+import Types exposing (..)
 import Update exposing (Msg(..))
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    div [ id "container" ]
+        [ navbar model.system
+        , div [] [ text <| "content will go here" ]
+        , footer
+        ]
+
+
+navbar : SystemOfMeasurement -> Html Msg
+navbar system =
+    nav [ class "navbar", attribute "role" "navigation" ]
+        [ div [ class "navbar-brand" ]
+            [ div [ class "navbar-item" ]
+                [ p [ class "title" ]
+                    [ span [ class "icon" ] [ i [ class "fa fa-film" ] [] ]
+                    , span [ class "app-title" ] [ text "film figures" ]
+                    ]
+                ]
+            ]
+        , div [ class "navbar-menu" ]
+            [ div [ class "navbar-start" ]
+                [ div [ class "navbar-item" ]
+                    [ p [ class "is-size-6" ] [ em [] [ text "a calculator for analogue film" ] ]
+                    ]
+                ]
+            , div [ class "navbar-end" ] [ text "measurement controls here" ]
+            ]
+        ]
+
+
+footer : Html Msg
+footer =
+    div [ class "level app-footer" ]
+        [ div [ class "level-left" ] [ text "app by KFN" ]
+        , div [ class "is-size-6" ] [ text "links here" ]
         ]
