be394d1e3751f0b4f8ccdb76ad5d276fcedbb9fe
kfrn <knfrances@gmail.com>
2018-02-17 11:32:42 +1300

    3: Update tests to include different speeds

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -23,14 +23,14 @@ all =
                     \_ ->
                         Expect.equal frameCount 64
                 ]
-            , describe "gauge = 35mm, footage = 576', speed = 24fps" <|
+            , describe "gauge = 35mm, footage = 576', speed = 25fps" <|
                 let
                     ( duration, frameCount ) =
-                        fromFootage TwentyFourFPS ThirtyFive 576
+                        fromFootage TwentyFiveFPS ThirtyFive 576
                 in
                 [ test "duration is correct" <|
                     \_ ->
-                        Expect.equal duration 384
+                        Expect.equal duration 368.64
                 , test "framecount is correct" <|
                     \_ ->
                         Expect.equal frameCount 9216
@@ -47,14 +47,14 @@ all =
                     \_ ->
                         Expect.equal frameCount 10000
                 ]
-            , describe "gauge = 9.5mm, footage = 250', speed = 24fps" <|
+            , describe "gauge = 9.5mm, footage = 250', speed = 16fps" <|
                 let
                     ( duration, frameCount ) =
-                        fromFootage TwentyFourFPS NinePtFive 250
+                        fromFootage SixteenFPS NinePtFive 250
                 in
                 [ test "duration is correct" <|
                     \_ ->
-                        Expect.equal (Round.round 3 duration) (toString 416.667)
+                        Expect.equal duration 625
                 , test "framecount is correct" <|
                     \_ ->
                         Expect.equal frameCount 10000
@@ -71,14 +71,14 @@ all =
                     \_ ->
                         Expect.equal frameCount 2880
                 ]
-            , describe "gauge = 28mm, footage = 120', speed = 24fps" <|
+            , describe "gauge = 28mm, footage = 120', speed = 18fps" <|
                 let
                     ( duration, frameCount ) =
-                        fromFootage TwentyFourFPS TwentyEight 120
+                        fromFootage EighteenFPS TwentyEight 120
                 in
                 [ test "duration is correct" <|
                     \_ ->
-                        Expect.equal duration 102.5
+                        Expect.equal (Round.round 3 duration) (toString 136.667)
                 , test "framecount is correct" <|
                     \_ ->
                         Expect.equal frameCount 2460
@@ -105,23 +105,23 @@ all =
                 , test "footage is correct" <|
                     \_ -> Expect.equal footage 10
                 ]
-            , describe "gauge = 16mm, frame count = 1840, speed = 24fps" <|
+            , describe "gauge = 16mm, frame count = 1840, speed = 29.97fps" <|
                 let
                     ( duration, footage ) =
-                        fromFrameCount TwentyFourFPS Sixteen 1840
+                        fromFrameCount NtscFPS Sixteen 1840
                 in
                 [ test "duration is correct" <|
-                    \_ -> Expect.equal (Round.round 3 duration) (toString 76.667)
+                    \_ -> Expect.equal (Round.round 3 duration) (toString 61.395)
                 , test "footage is correct" <|
                     \_ -> Expect.equal footage 46
                 ]
-            , describe "gauge = 9.5mm, frame count = 1840, speed = 24fps" <|
+            , describe "gauge = 9.5mm, frame count = 1840, speed = 16fps" <|
                 let
                     ( duration, footage ) =
-                        fromFrameCount TwentyFourFPS NinePtFive 1840
+                        fromFrameCount SixteenFPS NinePtFive 1840
                 in
                 [ test "duration is correct" <|
-                    \_ -> Expect.equal (Round.round 3 duration) (toString 76.667)
+                    \_ -> Expect.equal duration 115
                 , test "footage is correct" <|
                     \_ -> Expect.equal footage 46
                 ]
@@ -147,35 +147,35 @@ all =
                 , test "footage is correct" <|
                     \_ -> Expect.equal footage 120
                 ]
-            , describe "gauge = 16mm, duration = 30 seconds, speed = 24fps" <|
+            , describe "gauge = 16mm, duration = 30 seconds, speed = 25fps" <|
                 let
                     ( frameCount, footage ) =
-                        fromDuration TwentyFourFPS Sixteen 30
+                        fromDuration TwentyFiveFPS Sixteen 30
                 in
                 [ test "frame count is correct" <|
-                    \_ -> Expect.equal frameCount 720
+                    \_ -> Expect.equal frameCount 750
                 , test "footage is correct" <|
-                    \_ -> Expect.equal footage 18
+                    \_ -> Expect.equal footage 18.75
                 ]
-            , describe "gauge = 16mm, duration = 576 seconds, speed = 24fps" <|
+            , describe "gauge = 16mm, duration = 576 seconds, speed = 29.97fps" <|
                 let
                     ( frameCount, footage ) =
-                        fromDuration TwentyFourFPS Sixteen 576
+                        fromDuration NtscFPS Sixteen 576
                 in
                 [ test "frame count is correct" <|
-                    \_ -> Expect.equal frameCount 13824
+                    \_ -> Expect.equal (Round.round 2 frameCount) (toString 17262.74)
                 , test "footage is correct" <|
-                    \_ -> Expect.equal footage 345.6
+                    \_ -> Expect.equal (Round.round 3 footage) (toString 431.568)
                 ]
-            , describe "gauge = 9.5mm, duration = 576 seconds, speed = 24fps" <|
+            , describe "gauge = 9.5mm, duration = 576 seconds, speed = 16fps" <|
                 let
                     ( frameCount, footage ) =
-                        fromDuration TwentyFourFPS NinePtFive 576
+                        fromDuration SixteenFPS NinePtFive 576
                 in
                 [ test "frame count is correct" <|
-                    \_ -> Expect.equal frameCount 13824
+                    \_ -> Expect.equal frameCount 9216
                 , test "footage is correct" <|
-                    \_ -> Expect.equal footage 345.6
+                    \_ -> Expect.equal footage 230.4
                 ]
             , describe "gauge = 28mm, duration = 576 seconds, speed = 24fps" <|
                 let
